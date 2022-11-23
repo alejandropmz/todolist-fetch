f113ed2382c6f0cee31d14886c273402c35c8360
alejandropmz <alejandropacheco2497@gmail.com>
2022-11-23 02:51:29 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [tasks, setTasks] = useState([
+    "tarea1",
+    "tarea2"
+  ]);
+
+  const [newTasks, setNewTask] = useState("")
+
+  function addTasks(e){
+    if(e.code=="Enter" && newTasks!=""){
+      setTasks([...tasks, newTasks])
+      setNewTask("")
+    }
+  }
+
+  function removeTask(index){
+    tasks.splice(index, 1)
+    console.log(tasks)
+    setNewTask([...newTasks])
+  }
+
+  return (
+    <div className="container w-50 d-flex justify-content-center">
+      <ul className="list-group">
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+          <input
+            type="text"
+            className="form-control" onKeyDown={e=>addTasks(e)}
+            onChange={e=>setNewTask(e.target.value)}
+            value={newTasks}
+            aria-label="Text input with dropdown button"
+          ></input>
+        </li>
+        {tasks.map((task, index) => (
+          <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+            {task}
+            <button className="badge bg-danger rounded-pill" onClick={()=>removeTask(index)}>X</button>
+          </li>
+        ))}
+        <li className="list-group-item d-flex text-center justify-content-center align-items-center">
+          <small>{tasks.length} items</small>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
